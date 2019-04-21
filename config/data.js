084297bf00b327c8b3949a3e78c8ae78a175dc39
Kevin Giraldo <kevinngiraldo@gmail.com>
2019-04-21 15:53:01 -0400

    Update data.js

--- config/data.js
+++ config/data.js
@@ -1,7 +1,5 @@
 module.exports = {
-	MongoConnString: 'mongodb://130.245.170.206:27017/soc2',
-	MongoConnStringDev: 'mongodb://localhost:27017/soc2',
-	dev: 'http://130.245.170.206/',
-	PROD: 'mongodb://192.168.122.34:27017/soc'
+	MONGO_DEV: 'mongodb://130.245.170.206:27017/soc',
+	MONGO_PROD: 'mongodb://192.168.122.34:27017/soc'
 
 };
