4bf99f014f0ea12fa4e3c0211f3a5951bf0f0578
Kevin Giraldo <kevinngiraldo@gmail.com>
2019-04-21 15:37:41 -0400

    added missing comma

--- config/data.js
+++ config/data.js
@@ -2,7 +2,7 @@ module.exports = {
 	MongoConnString: 'mongodb://130.245.170.206:27017/soc2',
 	MongoConnStringDev: 'mongodb://localhost:27017/soc2',
 	dev: 'http://130.245.170.206/',
-	PROD: 'mongodb://192.168.122.34:27017/soc'
+	PROD: 'mongodb://192.168.122.34:27017/soc',
 	backDoorKey: 'abracadabra',
 	GmailPass: 'Stack12123.'
 };
